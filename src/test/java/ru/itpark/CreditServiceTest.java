c76170da406601d64f828fe68c4ac5cfb5759df3
NailMukhametshin <nail.zubr@gmail.com>
2018-12-12 21:30:09 +0300

    @Test

--- src/test/java/ru/itpark/CreditServiceTest.java
+++ src/test/java/ru/itpark/CreditServiceTest.java
@@ -1,10 +1,12 @@
 package ru.itpark;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calculateCredit() {
         {
             CreditService creditService = new CreditService();
